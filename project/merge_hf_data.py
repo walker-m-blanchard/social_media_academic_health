fd193d5d119b0253e8c01f3166e14a8ceba8f550
Walker <walker.m.blanchard@gmail.com>
2025-10-04 22:27:53 -0400

    Finished pre-processing of lyrics

--- project/merge_hf_data.py
+++ project/merge_hf_data.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-lyrics_data = pd.read_csv("data/song_lyrics2.csv", usecols=['artist', 'title', 'lyrics'])
+lyrics_data = pd.read_csv("data/song_lyrics2.csv", usecols=['artist', 'title', 'lyrics', 'language'], header=0)
 num_data = pd.read_csv("data/dataset.csv")
 
-num_data = num_data.drop(columns=['track_id', 'album_name', 'Unnamed: 0'])
+# Drops column used to number the songs from 1...
+num_data = num_data.drop(columns=['album_name', 'Unnamed: 0'])
 
+# Merges data by artist and song, drops the duplicate columns, drops duplicate songs, and filters for English
 song_data = pd.merge(lyrics_data, num_data, left_on=['artist', 'title'], right_on=['artists', 'track_name'], how='inner')
-song_data = song_data.drop(columns=['artists', 'track_name'])
+song_data.drop(columns=['artists', 'track_name'], inplace=True)
 song_data.drop_duplicates(subset=['artist', 'title'], inplace=True)
+filtered_songs = song_data[song_data['language'] == 'en']
 
-song_data.to_csv("data/hf_data.csv", index=False)
+filtered_songs.to_csv("data/hf_data.csv", index=False)
